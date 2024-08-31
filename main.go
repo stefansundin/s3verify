00938f302f205c70f854a01b6682283476682fec
Stefan Sundin <git@stefansundin.com>
2024-08-31 16:51:22 -0700

    Migrate to EndpointResolverV2.

--- main.go
+++ main.go
@@ -180,7 +180,7 @@ func main() {
 				o.Region = region
 			}
 			if endpointURL != "" {
-				o.EndpointResolver = s3.EndpointResolverFromURL(endpointURL)
+				o.BaseEndpoint = aws.String(endpointURL)
 			}
 			if usePathStyle {
 				o.UsePathStyle = true
